28b7ad82febf78b01208b7c57c177c4b318a5b2c
yusufovamina <yusufovamina@gmail.com>
2026-05-10 20:57:00 +0400

    first commit

---
+++ cmd/server/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	// Базовые middleware
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Простой endpoint для проверки
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module expense_tracker_api
+
+go 1.23.0
+
+require (
+	github.com/go-chi/chi/v5 v5.0.12
+	github.com/mattn/go-sqlite3 v1.14.22
+)
